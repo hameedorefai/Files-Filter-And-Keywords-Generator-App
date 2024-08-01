0f51b84110bce85f8202a780ef9da071fb9d3e3a
Mahmoud Refai <98220943+hameedorefai@users.noreply.github.com>
2024-08-01 06:40:02 +0300

    Update clsFilteringProcess.cs

--- BusinessLogic/clsFilteringProcess.cs
+++ BusinessLogic/clsFilteringProcess.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
@@ -42,9 +41,7 @@ namespace BusinessLogic
                     List<string> keywords = Course.GetAllKeywords();
 
 
-                    List<string> normalizedKeywords = keywords
-                        .Select(k => Regex.Replace(k, @"[\W_]+", "").ToLower())
-                        .ToList();
+                    List<string> normalizedKeywords = keywords.Select(k => Regex.Replace(k, @"[\W_]+", "").ToLower()).ToList();
 
                     var filteredFiles = AllFiles.Where(file =>
                     {
@@ -104,112 +101,3 @@ namespace BusinessLogic
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
-namespace BusinessLogic
-{
-    public class clsFilteringProcess
-    {
-
-
-        static public int FilterFiles()
-        {
-            int FilteredFilesCounter = 0;
-
-            string SourceFolderPath = clsPath.GetSourceFolderPath();
-
-            DataTable Courses = clsCourse.GetAllCoursesInfo();
-            foreach (DataRow courseRow in Courses.Rows)
-            {
-                try
-                {
-                     int CourseID = Convert.ToInt32(courseRow["CourseID"]);
-                    // int CourseID = 362;
-
-
-                    string SubCourseFolderPath = clsCourse.GetCoursePathByCourseID(CourseID);
-                    if (!Directory.Exists(SubCourseFolderPath))
-                    {
-
-                        Directory.CreateDirectory(SubCourseFolderPath);
-                    }
-
-                    var AllFiles = Directory.GetFiles(SourceFolderPath);
-                    clsCourse Course = clsCourse.Find(CourseID);
-                    List<string> keywords = Course.GetAllKeywords();
-                    var filteredFiles = AllFiles.Where(file => keywords.Any(keyword => Path.GetFileName(file).IndexOf(keyword, StringComparison.OrdinalIgnoreCase) >= 0));
-
-
-
-
-                    string fileName = "";
-                    foreach (var file in filteredFiles)
-                    {
-                        try
-                        {
-                            fileName = Path.GetFileNameWithoutExtension(file);
-
-                            File.Copy(file, Path.Combine(SubCourseFolderPath, fileName), true);
-
-
-
-                            if (File.Exists(Path.Combine(SubCourseFolderPath, fileName)))
-                            {
-                                clsOperationLog.CreateNewOperationLog(CourseID, fileName, "Succeed", "Adding Files");
-                                FilteredFilesCounter++;
-                            }
-                            else
-                            {
-                                try
-                                {
-                                    throw new Exception("File does not exist: " + Path.Combine(SubCourseFolderPath, fileName));
-                                }
-                                catch (Exception ex)
-                                {
-                                    clsOperationLog.CreateNewOperationLog(CourseID, fileName, "Failed", "Adding Files", ex.Message);
-                                    if (FilteredFilesCounter == 0)
-                                    {
-                                        FilteredFilesCounter = -1; // هذا اللوجيك غير دقيقة سأعود لهذا الأمر لاحقاً.
-                                    }
-
-                                }
-                            }
-
-
-
-                        }
-                        catch (Exception ex)
-                        {
-                            clsOperationLog.CreateNewOperationLog(CourseID, fileName, "Failed", "Adding Files", ex.Message);
-                            if (FilteredFilesCounter == 0)
-                            {
-                                FilteredFilesCounter = -1; // هذا اللوجيك غير دقيقة سأعود لهذا الأمر لاحقاً.
-                            }
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    if (FilteredFilesCounter == 0)
-                    {
-                        FilteredFilesCounter = -1;
-                    }
-                }
-                break;// temporary
-            }
-
-            return FilteredFilesCounter;
-        }
-
-
-        }
-}
->>>>>>> 9052b7e699351b2d3f6cbab3cd02523c129d58e9
