3163d0626546a1e80645a745a82fbbdefa244b51
Mahmoud Refai <98220943+hameedorefai@users.noreply.github.com>
2024-08-06 03:46:52 +0300

    Update clsFilteringProcess.cs

--- BusinessLogic/clsFilteringProcess.cs
+++ BusinessLogic/clsFilteringProcess.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
@@ -8,23 +8,29 @@ using System.Windows.Forms;
 
 namespace BusinessLogic
 {
-
     public class clsFilteringProcess
     {
-        static public int FilterFiles()
+        static public int[] FilterFiles()
         {
+
             int LocalCourseID = -1;
             string LocalFileName = "";
-
+            float LocalFileLength = 0;
             int FilteredFilesCounter = 0;
+            int FileswithGUID = 0;
+            int RepeatedAndDeletedFiles = 0;
+            int AllFilesCount = 0;
+
             string SourceFolderPath = clsPath.GetSourceFolderPath();
             DataTable Courses = clsCourse.GetAllCoursesInfo();
+            var AllFiles = Directory.GetFiles(SourceFolderPath, "*.*", SearchOption.AllDirectories);
 
             foreach (DataRow courseRow in Courses.Rows)
             {
                 try
                 {
-                    int CourseID = Convert.ToInt32(courseRow["CourseID"]);
+                          int CourseID = Convert.ToInt32(courseRow["CourseID"]);
+               //     int CourseID = 309;
                     LocalCourseID = CourseID;
                     string SubCourseFolderPath = clsCourse.GetCoursePathByCourseID(CourseID);
 
@@ -34,52 +40,76 @@ namespace BusinessLogic
                     }
 
                     // temporary
-                   // SubCourseFolderPath = SubCourseFolderPath + @"\MidtermExams"; // here, I can add the sub file. (inside course file)
+                    // SubCourseFolderPath = SubCourseFolderPath + @"\MidtermExams"; // here, I can add the sub file. (inside course file)
 
-                                        var AllFiles = Directory.GetFiles(SourceFolderPath);
+                     AllFilesCount = AllFiles.Count();
                     clsCourse Course = clsCourse.Find(CourseID);
                     List<string> keywords = Course.GetAllKeywords();
-
 
                     List<string> normalizedKeywords = keywords.Select(k => Regex.Replace(k, @"[\W_]+", "").ToLower()).ToList();
 
                     var filteredFiles = AllFiles.Where(file =>
                     {
-
                         string normalizedFileName = Regex.Replace(Path.GetFileNameWithoutExtension(file), @"[\W_]+", "").ToLower();
                         return normalizedKeywords.Any(keyword => normalizedFileName.Contains(keyword));
                     });
 
+                    if (filteredFiles == null)
+                         continue;
+
                     foreach (var file in filteredFiles)
                     {
+                        float FileLength = file.Length;
+                        LocalFileLength = FileLength;
                         try
                         {
-                            string fileName = Path.GetFileNameWithoutExtension(file); 
+                            string fileName = Path.GetFileNameWithoutExtension(file);
                             LocalFileName = fileName;
                             string fileExtension = Path.GetExtension(file);
                             string destinationFilePath = Path.Combine(SubCourseFolderPath, fileName + fileExtension);
 
-                              File.Copy(file, destinationFilePath, true);
-                          //  File.Move(file, destinationFilePath);
-
                             if (File.Exists(destinationFilePath))
                             {
-                                clsOperationLog.CreateNewOperationLog(CourseID, fileName, "Succeed", "Adding Files");
-                                FilteredFilesCounter++;
+                                FileInfo sourceFileInfo = new FileInfo(file);
+                                FileInfo destinationFileInfo = new FileInfo(destinationFilePath);
+
+                                if (sourceFileInfo.Length != destinationFileInfo.Length)
+                                {
+                                    string newFileName = fileName + "_GUID:" + Guid.NewGuid().ToString() + fileExtension;
+                                    string newDestinationFilePath = Path.Combine(SubCourseFolderPath, newFileName);
+
+                                     File.Move(file, newDestinationFilePath);
+                                   // File.Copy(file, newDestinationFilePath);
+                                    clsOperationLog.CreateNewOperationLog(CourseID, newFileName, "Succeed", "Adding GUID Files", FileLength);
+                                    FilteredFilesCounter++;
+                                    FileswithGUID++;
+                                }
+                                else
+                                {
+                                    string repeatedFilesFolderPath = @"D:\ExportetData\RepeatedFiles";
+                                    if (!Directory.Exists(repeatedFilesFolderPath))
+                                    {
+                                        Directory.CreateDirectory(repeatedFilesFolderPath);
+                                    }
+
+                                    // Delete the file in the source folder or move it to a new and independ folder.
+                                    File.Delete(file);
+                                    clsOperationLog.CreateNewOperationLog(CourseID, fileName, "Succeed", "Removing Files", FileLength);
+                                    RepeatedAndDeletedFiles++;
+                                }
                             }
                             else
                             {
-                                clsOperationLog.CreateNewOperationLog(CourseID, fileName, "Failed", "Adding Files", "");
-                                if (FilteredFilesCounter == 0)
-                                {
-                                    FilteredFilesCounter = -1; // هذا اللوجيك غير دقيق سأعود له لاحقاً.
-                                }
+                                 File.Move(file, destinationFilePath);
+                               // File.Copy(file, destinationFilePath, true);
+                                clsOperationLog.CreateNewOperationLog(CourseID, fileName, "Succeed", "Adding Files", file.Length);
+                                FilteredFilesCounter++;
                             }
                         }
                         catch (Exception ex)
                         {
                             string fileName = Path.GetFileNameWithoutExtension(file);
-                            clsOperationLog.CreateNewOperationLog(CourseID, fileName, "Failed", "Adding Files", ex.Message);
+                            clsOperationLog.CreateNewOperationLog(CourseID, fileName, "Failed", "Adding Files", FileLength,ex.Message);
                             if (FilteredFilesCounter == 0)
                             {
                                 FilteredFilesCounter = -1; // هذا اللوجيك غير دقيق سأعود له لاحقاً.
@@ -93,11 +123,41 @@ namespace BusinessLogic
                     {
                         FilteredFilesCounter = -1;
                     }
-                    clsOperationLog.CreateNewOperationLog(LocalCourseID, LocalFileName, "Failed", "Adding Files", ex.Message);
+                    clsOperationLog.CreateNewOperationLog(LocalCourseID, LocalFileName, "Failed", "Adding Files", LocalFileLength, ex.Message);
                 }
             }
-
-            return FilteredFilesCounter;
+            int[] arrData = new int[4];
+            arrData[0] = FilteredFilesCounter;
+            arrData[1] = FileswithGUID;
+            arrData[2] = RepeatedAndDeletedFiles;
+            arrData[3] = AllFilesCount;
+            return arrData;
         }
+
+
+        static void DeleteAllImages(string folderPath)
+        {
+            string[] imageExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".ico" };
+
+            try
+            {
+                var allFiles = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+
+                foreach (var file in allFiles)
+                {
+                    string fileExtension = Path.GetExtension(file).ToLower();
+
+                    if (Array.Exists(imageExtensions, ext => ext.Equals(fileExtension)))
+                    {
+                        File.Delete(file);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+
     }
 }
